234f13062cc003049682fde9340bbe44d450736c
Daniel <65187330+STRIXBG@users.noreply.github.com>
2024-03-07 13:32:47 +0200

    Update index_operators.php

--- PHP Web Programming, 3 Course/Lesson 1/index_operators.php
+++ PHP Web Programming, 3 Course/Lesson 1/index_operators.php
@@ -58,10 +58,15 @@
         // [,]
         // [=>]
         
-        // if ()
+        // if
         // else
         // elseif
-        //switch-case
+        // switch-case
+        // while
+        // do-while
+        // foreach
+        // for
+        // continue
         
         if ($a > $b) {
             echo '$a is greater than $b';
@@ -85,9 +90,49 @@
             }
             default:
             {
-                echo '<br><br>not 2 or 3';
+                echo '<br><br>$a is NOT 2 or 3<hr>';
                 break;
             }
+        }
+        
+        echo '<br><br>While:';
+        $i = 13;
+        while ($i > 10)
+        {
+            echo '<br>'.$i;
+            $i -= 1;
+        }
+        
+        echo '<br><br>Do-While:';
+        $i2 = 13;
+        do
+        {
+            echo '<br>'.$i2;
+            $i2 -= 1;
+        } while ($i2 > 10);
+        
+        echo '<br><br>Foreach:';
+        $arr = array(1, 2, 3, 4, 'Ivan', 'Georgi');
+        foreach ($arr as &$value) {
+            echo '<br>' . $value;
+        }
+        
+        echo '<br><br>For:';
+        for ($i3 = 0; $i3 < 10; $i3++)
+        {
+            echo '<br>' . $i3;
+        }
+        
+        echo '<br><br>Continue:';
+        for ($i3 = 0; $i3 < 2; $i3++)
+        {
+            $continue_ = random_int(false, true);
+            if ($continue_)
+            {
+                echo '<br>Continue!!';
+                continue;
+            }
+            echo '<br>No Continue';
         }
         ?>
     </body>
