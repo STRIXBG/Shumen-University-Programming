60d2429d5ed9154724ef98e08a74348d9c1c8493
Daniel <65187330+STRIXBG@users.noreply.github.com>
2024-03-07 11:45:26 +0200

    Create index_vars.php

---
+++ PHP Web Programming, 3 Course/Lesson 1/index_vars.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Упражнения по Уеб Програмиране (Урок: 1 - Константи, Променливи)</title>
+    </head>
+    <body>
+        <?php
+        // INFO: define("CONST NAME", "value");
+        // INFO: not case senstive
+        // 
+        // Define Constants:
+        define("PI", 3.14);
+        define("STR", "STRING CONST");
+        echo 'PI: ' . PI . '<br>';
+        echo 'STR: ' . STR . '<br>';
+
+        $a = 9.50 * sin(2 * PI / 9) + 6;
+        echo 'RESULT = ' . $a . '<br>';
+
+        // INFO: 4 TYPES VARIABLES:
+        // SUPER GLOBAL VARS, 
+        // GLOBAL VARS (without functions),
+        // LOCAL VARS (only in functions),
+        // GLOBAL IN FUNCTION (with construction global) points at global vars with same names
+        // TESTING
+        $a_ = 2;
+        $b_ = 3;
+        $c_ = 3;
+
+        function sum() {
+            global $a_, $b_, $c_;
+
+            $c_ = $a_ + $b_;
+        }
+
+        sum();
+        echo '<br>Sum Function:<br>' . $a_ . '<br>' . $b_ . '<br>' . $c_;
+        ?>
+    </body>
+</html>
