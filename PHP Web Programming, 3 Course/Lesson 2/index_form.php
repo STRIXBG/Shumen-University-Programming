5315bc0f6e9c661dcedc2b2c9c8aafb3e8e74273
Daniel <65187330+STRIXBG@users.noreply.github.com>
2024-03-18 10:49:00 +0200

    Create index_form.php

---
+++ PHP Web Programming, 3 Course/Lesson 2/index_form.php
@@ -0,0 +1,28 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Упражнения по Уеб Програмиране (Урок: 2)</title>
+</head>
+
+<body>
+    <h4>Shop</h4>
+    <form action="request_1.php">
+        <select name="item">
+            <option>Пъзел</option>
+            <option>Топка</option>
+            <option>Конзола</option>
+            <option>Играчка</option>
+            <option>Настолна игра</option>
+        </select>
+
+        <input type="number" name="quanity"/>
+        <input type="submit" name="submit" value="Puruchase"/>
+    </form>
+    <?php
+
+    ?>
+</body>
+
+</html>
