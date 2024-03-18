037632aaeb1311be60d2cbf9ec25ed19d14e9cad
Daniel <65187330+STRIXBG@users.noreply.github.com>
2024-03-18 11:08:41 +0200

    Create index_login.php

---
+++ PHP Web Programming, 3 Course/Lesson 2/index_login.php
@@ -0,0 +1,33 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Упражнения по Уеб Програмиране (Урок: 2)</title>
+</head>
+
+<body>
+
+    <h2 align="center">Login</h2>
+
+    <form action="request_login.php" name="form_login" method="POST">
+        <table align="center">
+            <tr>
+                <td>User:</td>
+                <td>Pass:</td>
+            </tr>
+
+            <tr>
+                <td> <input placeholder="Username" name="username" type="text" /> </td>
+                <td> <input placeholder="Password" name="password" type="password" /> 
+                <input type="submit" value="Submit">
+                </td>
+            </tr>
+        </table>
+    </form>
+    <?php
+
+    ?>
+</body>
+
+</html>
