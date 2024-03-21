a75e7a640ae22b029bf258bb7c86e466e5eeea53
Daniel <65187330+STRIXBG@users.noreply.github.com>
2024-03-21 09:52:31 +0200

    Create index_dates.php

---
+++ PHP Web Programming, 3 Course/Lesson 3/index_dates.php
@@ -0,0 +1,18 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Упражнения по Уеб Програмиране (Урок: 3)</title>
+</head>
+
+<body>
+    <?php
+    $today = date("y-m-d");
+    $time = date("H:i:s");
+
+    echo 'Today is: ' . $today . " and " . $time;
+    ?>
+</body>
+
+</html>
